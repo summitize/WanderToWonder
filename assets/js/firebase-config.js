e0b723653cff7fd40a6dde2b5a0b271d4c31d603
Sumeet Boob <Sumeet.Boob@BRILLIO.COM>
2026-05-15 21:16:04 +0530

    Next commit

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -5,12 +5,12 @@
  * Add authorized domains: wandertowonder.in, www.wandertowonder.in, summitize.github.io
  */
 window.WTW_FIREBASE_CONFIG = {
-    apiKey: 'PASTE_FIREBASE_API_KEY',
-    authDomain: 'PASTE_FIREBASE_AUTH_DOMAIN',
-    projectId: 'PASTE_FIREBASE_PROJECT_ID',
-    storageBucket: 'PASTE_FIREBASE_STORAGE_BUCKET',
-    messagingSenderId: 'PASTE_FIREBASE_MESSAGING_SENDER_ID',
-    appId: 'PASTE_FIREBASE_APP_ID'
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_PROJECT.firebaseapp.com',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_PROJECT.appspot.com',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    appId: 'YOUR_APP_ID'
 };
 
 window.WTW_FIREBASE_ENABLED = (() => {
